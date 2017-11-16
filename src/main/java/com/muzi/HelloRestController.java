5fb42050a0ddaf5718855e1ece62b148fcf5cd49
muzi.he <muzi.habo@gmail.com>
2017-11-16 22:35:18 +0800

    add controller params

--- src/main/java/com/muzi/HelloRestController.java
+++ src/main/java/com/muzi/HelloRestController.java
@@ -2,15 +2,14 @@ package com.muzi;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * Created by muzi on 2017/11/15.
  */
 
 @RestController
+@RequestMapping(value = "/boot")
 public class HelloRestController {
 
     @Value("${cupSize}")
@@ -25,8 +24,12 @@ public class HelloRestController {
     @Autowired
     private GirlProperties girlProperties;
 
-    @RequestMapping(value = "/hello-rest", method = RequestMethod.GET)
-    public String say() {
-        return girlProperties.getCupSize();
+    @RequestMapping(value = {"/{id}/hello-rest", "/rest-hello"}, method = RequestMethod.GET)
+
+    //获取url链接参数
+    //@PathVariable("id") Integer id
+    //@RequestParam(value = "id", required = false, defaultValue = "0") Integer id
+    public String say(@PathVariable("id") Integer id) {
+        return girlProperties.getCupSize() + id;
     }
 }
