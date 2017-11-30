ce6d1cb92d4d2a64d7bbc51b6d68e841815c02dd
muzi <Hw19921203>
2017-11-30 16:17:46 +0800

    添加统一异常处理类 http://blog.didispace.com/springbootexception/

--- src/main/java/com/muzi/controller/HelloController.java
+++ src/main/java/com/muzi/controller/HelloController.java
@@ -1,5 +1,7 @@
 package com.muzi.controller;
 
+import com.muzi.enums.ResultEnum;
+import com.muzi.exception.GirlException;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -14,5 +16,10 @@ public class HelloController {
     @RequestMapping(value = "/hello", method = RequestMethod.GET)
     public String say() {
         return "index";
+    }
+
+    @RequestMapping(value = "/hello_exception", method = RequestMethod.GET)
+    public String helloException() throws GirlException {
+        throw new GirlException(ResultEnum.UNKONW_ERROR);
     }
 }
--- src/main/java/com/muzi/handle/ExceptionHandle.java
+++ src/main/java/com/muzi/handle/ExceptionHandle.java
@@ -8,6 +8,9 @@ import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * Created by muzi on 2017/11/20.
@@ -29,5 +32,14 @@ public class ExceptionHandle {
             logger.info("[系统异常] {}", e);
             return ResultUtil.error(-1, "未知错误");
         }
+    }
+
+    @ExceptionHandler(value = GirlException.class)
+    public ModelAndView defaultExceptionHandle(HttpServletRequest request, GirlException e) {
+        ModelAndView mav = new ModelAndView();
+        mav.addObject("exception", e);
+        mav.addObject("url", request.getRequestURL());
+        mav.setViewName("error");
+        return mav;
     }
 }
